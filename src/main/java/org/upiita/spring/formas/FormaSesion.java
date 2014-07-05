9664589f4b8c9839c7193f8bcba07df2de1632f6
Geovanni1983 <geomarir@hotmail.com>
2014-07-05 12:49:13 -0500

    Se sube primera version del blog

---
+++ src/main/java/org/upiita/spring/formas/FormaSesion.java
@@ -0,0 +1,15 @@
+package org.upiita.spring.formas;
+
+//@Scope("session")
+public class FormaSesion {
+
+		private Integer itemsComprados=0;
+
+		public Integer getItemsComprados() {
+			return itemsComprados;
+		}
+
+		public void setItemsComprados(Integer itemsComprados) {
+			this.itemsComprados = itemsComprados;
+		}
+}
